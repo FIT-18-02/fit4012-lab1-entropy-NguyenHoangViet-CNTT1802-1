d9d223ad84472bc2429bb082465e42012d3d133f
NguyenHoangViet-CNTT1802 <hoangviet11t1@gmail.com>
2026-04-16 20:32:07 +0700

    Update mod_inverse.cpp

--- src/mod_inverse.cpp
+++ src/mod_inverse.cpp
@@ -1,15 +1,5 @@
 #include <iostream>
-
 using namespace std;
-
-int gcd(int a, int b) {
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
-}
 
 int extended_euclid(int a, int b, int &x, int &y) {
     if (b == 0) {
@@ -18,10 +8,12 @@ int extended_euclid(int a, int b, int &x, int &y) {
         return a;
     }
 
-    int x1 = 0, y1 = 0;
+    int x1, y1;
     int g = extended_euclid(b, a % b, x1, y1);
+
     x = y1;
     y = x1 - (a / b) * y1;
+
     return g;
 }
 
@@ -33,21 +25,22 @@ int mod_inverse(int a, int m) {
         return -1;
     }
 
-    return (x % m + m) % m;
+    // chuẩn hóa kết quả về [0, m-1]
+    x = (x % m + m) % m;
+    return x;
 }
+
 int main() {
-    int a = 0, m = 0;
-    cout << "Nhap a, m: ";
+    int a, m;
     cin >> a >> m;
 
-    if (gcd(a, m) != 1) {
-        cout << "Khong ton tai nghich dao modulo vi gcd(a, m) != 1.\n";
-        return 0;
+    int inv = mod_inverse(a, m);
+
+    if (inv == -1) {
+        cout << -1 << endl;
+    } else {
+        cout << inv << endl;
     }
 
-    int inv = mod_inverse(a, m);
-    cout << "Nghich dao cua " << a << " mod " << m << " la: " << inv << '\n';
-    cout << "Kiem tra: " << a << " * " << inv << " % " << m
-         << " = " << (1LL * a * inv % m) << '\n';
     return 0;
 }
